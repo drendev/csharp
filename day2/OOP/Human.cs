1f1c84d4c26fc3c3ab95c0ae6c4b9592e1751637
drendev <91354016+drendev@users.noreply.github.com>
2025-08-03 17:51:50 +0800

    day 2 commits
    properties and costructors

---
+++ day2/OOP/Human.cs
@@ -0,0 +1,25 @@
+﻿
+namespace OOP
+{
+    public class Human
+    {
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hello, I'm {0} {1}. The color of my eyes is {2}, and I'm {3} years old", 
+                firstName, lastName, eyeColor, age);
+        }
+    }
+}
---
+++ day2/OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human aldren = new Human("Aldren", "Mendoza", "Brown", 26);
+            aldren.IntroduceMyself();
+            List<int> age1 = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (int item in age1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
