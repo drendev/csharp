77e50ce5c88211e59cbadde301c72d17f648e020
drendev <91354016+drendev@users.noreply.github.com>
2025-08-03 13:58:24 +0800

    Day 2 of refresh coding with C#

---
+++ day2/Vocabualry/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Vocabulary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+// #error version
+            Console.WriteLine($"Computer named {Env.MachineName}");
+        }
+    }
+}
